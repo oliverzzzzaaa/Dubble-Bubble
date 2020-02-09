2adfe24c5e7d1e42f71652f2c2bcef220cc253be
Oliver <oliverzzzzaaa@gmail.com>
2020-02-08 17:07:56 -0800

    fixed some collision

--- src/collision.js
+++ src/collision.js
@@ -30,14 +30,7 @@ function iscollideRectangle(circle, rectangle) {
 
 }
 
-    // var distX = Math.abs(circle.x - rect.x-rect.w/2);
-    // var distY = Math.abs(circle.y - rect.y-rect.h/2);
-
-    // if (distX > (rect.w/2 + circle.r)) { return false; }
-    // if (distY > (rect.h/2 + circle.r)) { return false; }
-
-    // if (distX <= (rect.w/2)) { return true; } 
-    // if (distY <= (rect.h/2)) { return true; }
+    
 
 
 
